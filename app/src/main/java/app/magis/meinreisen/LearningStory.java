008339b945e4ed58d8805884eb2fd9ea91cf2681
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-16 19:12:33 +0700

    add learning story

--- app/src/main/java/app/magis/meinreisen/LearningStory.java
+++ app/src/main/java/app/magis/meinreisen/LearningStory.java
@@ -18,26 +18,33 @@ import java.util.ArrayList;
  * Created by rizalsidikp on 16/09/17.
  */
 
-public class LearningBenda extends AppCompatActivity implements View.OnClickListener {
+public class LearningStory extends AppCompatActivity implements View.OnClickListener {
 
-    private final int REQ_CODE_SPEECH_INPUT = 100;
     MediaPlayer sound;
     int idx = 0;
-    Button next, prev, play, speak;
-    TextView textTampil, result;
-    Boolean hasilnya = false;
-    String[] kataKerja = {
-            "das Motorrad",
-            "das Schiff",
-            "der Fahrrad",
-            "der Zug",
-            "das Flugzeug",
-            "das Auto",
-            "der Bus"
+    Button next, prev, play;
+    TextView storyTampil, storyKe;
+    String[] story_ke = {
+            "Story 1",
+            "Story 2",
+            "Story 3",
+            "Story 4",
+            "Story 5"
     };
 
-    protected void setWord(){
-        textTampil.setText(kataKerja[idx]);
+    String[] story_tampil = {
+            "Thomas reist von den Niederlanden nach Deutschland. Er fährt mit dem Zug. Im Progamm besucht er Berlin. Er macht dort einen Spaziergang durch das Riegierungsviertel. Thomas will das Parlament besictigen, ϋber einen Flohmarkt bummeln. Am Abend will er auch ins Theater gehen.",
+            "Ich fahren mit meinen Freunden nach Berlin. Wir fahren mit dem Auto zum Brandenburger Tor. Wir sind sehr aufgeregt. Denn es ist unser erstes Mal in Berlin.",
+            "Am 30.Februar machen wir eine Fahrt nach Rom. Wir fahren mit dem Zug um 11.30 Uhr ab. Wir sind um 15.00 Uhr am Hotel in Rom.",
+            "Marlene geht oft zum Hauptbahnhof. Denn sie arbeitet als Kartenverkäuferin am Bahnhof. Sie arbeitet da seit 4 Jahren.",
+            "Im Urlaub reisen meine Familie und ich nach Brunei. Wir fliegen mit dem Fluzeug, Da besuchen wir meiner Oma. Wir reisen dann zusammen nach Malaysia und Indonesien."
+    };
+
+    protected void setStroyKe(){
+        storyKe.setText(story_ke[idx]);
+    }
+    protected void setStroyTampil(){
+        storyTampil.setText(story_tampil[idx]);
     }
 
     protected void playSound(){
@@ -46,31 +53,23 @@ public class LearningBenda extends AppCompatActivity implements View.OnClickList
         }
         switch (idx){
             case 0:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.das_motorrad);
+                sound = MediaPlayer.create(LearningStory.this, R.raw.story_one);
                 sound.start();
                 break;
             case 1:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.das_schiff);
+                sound = MediaPlayer.create(LearningStory.this, R.raw.story_two);
                 sound.start();
                 break;
             case 2:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.der_fahrrad);
+                sound = MediaPlayer.create(LearningStory.this, R.raw.story_three);
                 sound.start();
                 break;
             case 3:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.der_zug);
+                sound = MediaPlayer.create(LearningStory.this, R.raw.story_four);
                 sound.start();
                 break;
             case 4:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.das_flugzeug);
-                sound.start();
-                break;
-            case 5:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.das_auto);
-                sound.start();
-                break;
-            case 6:
-                sound = MediaPlayer.create(LearningBenda.this, R.raw.der_bus);
+                sound = MediaPlayer.create(LearningStory.this, R.raw.story_five);
                 sound.start();
                 break;
             default:
@@ -78,97 +77,52 @@ public class LearningBenda extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private void tampilDialog(){
-        final Dialog dialog = new Dialog(LearningBenda.this);
-        dialog.setContentView(R.layout.dialog);
-        TextView tHasilnya = (TextView) dialog.findViewById(R.id.hasilnya);
-        tHasilnya.setText(String.valueOf(hasilnya));
-        dialog.show();
-    }
-
-    private void inputSpeechDialog(){
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//      dibawah ini adalah settingan untuk memanggil google speech input
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "de-DE");
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, kataKerja[idx]);
-
-//      ini adalah perintah untuk mengeksekusi intent dan memunculkan dialog speech input google
-        try {
-            startActivityForResult(i, REQ_CODE_SPEECH_INPUT);
-        }catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(), getString(R.string.tidaksupport), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    //  ini function ketika menerima hasil dari dialog google speech input
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if(resultCode == RESULT_OK && null != data){ //kalau resultCode nya sama dengan OK
-//                  masukkan hasil suara kedalam array (biar bisa lebih dari 1 kata)
-                    ArrayList<String> hasil = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-//                  masukkan hasil array ke text
-                    result.setText(hasil.get(0));
-                    if(hasil.get(0).equals(kataKerja[idx])){
-                        hasilnya = true;
-                    }else{
-                        hasilnya = false;
-                    }
-                    tampilDialog();
-                }
-                break;
-            }
-        }
-    }
-
 
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learning);
-        textTampil = (TextView) findViewById(R.id.text_tampil);
-        result = (TextView) findViewById(R.id.result);
+        setContentView(R.layout.activity_story);
+        storyKe = (TextView) findViewById(R.id.story_ke);
+        storyTampil = (TextView) findViewById(R.id.story_tampil);
         next = (Button) findViewById(R.id.next);
         prev = (Button) findViewById(R.id.prev);
         play = (Button) findViewById(R.id.play);
-        speak = (Button) findViewById(R.id.speak);
-        setWord();
+        setStroyKe();
+        setStroyTampil();
 
         next.setOnClickListener(this);
         prev.setOnClickListener(this);
         play.setOnClickListener(this);
-        speak.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.next:
-                if(idx == 6){
+                if(idx == 4){
                     idx = 0;
                 }else{
                     idx++;
                 }
-                setWord();
+                if(sound != null){
+                    sound.stop();
+                }
+                setStroyKe();
+                setStroyTampil();
                 break;
             case R.id.prev:
                 if(idx == 0){
-                    idx = 6;
+                    idx = 4;
                 }else {
                     idx--;
                 }
-                setWord();
+                if(sound != null){
+                    sound.stop();
+                }
+                setStroyKe();
+                setStroyTampil();
                 break;
             case R.id.play:
                 playSound();
-                break;
-            case R.id.speak:
-                inputSpeechDialog();
                 break;
             default:
                 break;
