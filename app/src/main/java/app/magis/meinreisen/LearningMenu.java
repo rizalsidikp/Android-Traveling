cf5aadf3e8731cc45c7642a32c002e9ba09d1ee3
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-16 17:26:25 +0700

    add learning menu activity

--- app/src/main/java/app/magis/meinreisen/LearningMenu.java
+++ app/src/main/java/app/magis/meinreisen/LearningMenu.java
@@ -1,10 +1,34 @@
 package app.magis.meinreisen;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by rizalsidikp on 16/09/17.
  */
 
-public class LearningMenu extends AppCompatActivity {
+public class LearningMenu extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_menu);
+        LinearLayout kerja = (LinearLayout) findViewById(R.id.kerja);
+        kerja.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.kerja:
+                Intent i = new Intent(LearningMenu.this, LearningKerja.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
+    }
 }
