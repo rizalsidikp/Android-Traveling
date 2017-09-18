c8be9d5576cf13f5b6e1ad83dd6bef5578553b97
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 18:26:28 +0700

    add new file

---
+++ app/src/main/java/app/magis/meinreisen/DBOpenHelper.java
@@ -0,0 +1,28 @@
+package app.magis.meinreisen;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by rizalsidikp on 18/09/17.
+ */
+
+public class DBController extends SQLiteOpenHelper {
+
+
+    public DBController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table levels (_id integer primary key autoincrement, level int, status int)");
+        db.execSQL("insert into levels (level, status) values (2,0), (3,0)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
