9d331eac2a47504415f609f4de7f4f803803899c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 18:27:33 +0700

    add conditional level

--- app/src/main/java/app/magis/meinreisen/QuizMenu.java
+++ app/src/main/java/app/magis/meinreisen/QuizMenu.java
@@ -1,6 +1,10 @@
 package app.magis.meinreisen;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,16 +13,52 @@ import android.widget.LinearLayout;
 
 public class QuizMenu extends AppCompatActivity implements View.OnClickListener {
 
+    LinearLayout level_one, level_two, level_three;
+
+    protected void setLevel(){
+        Connector con = new Connector(this);
+        con.open();
+        Cursor rs = con.database.rawQuery("select * from levels where level = 2", null);
+        if(rs.moveToFirst()){
+            if(rs.getInt(rs.getColumnIndex("status")) == 1){
+                level_two.setOnClickListener(this);
+                level_two.setBackgroundColor(Color.parseColor("#00FFFF"));
+            }else{
+                level_two.setBackgroundColor(Color.LTGRAY);
+            }
+        }
+        rs.close();
+        rs = con.database.rawQuery("select * from levels where level = 3", null);
+        if(rs.moveToFirst()){
+            if(rs.getInt(rs.getColumnIndex("status")) == 1){
+                level_three.setOnClickListener(this);
+                level_three.setBackgroundColor(Color.parseColor("#FF00FF"));
+            }else{
+                level_three.setBackgroundColor(Color.LTGRAY);
+            }
+        }
+        rs.close();
+        con.close();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_vertical);
-        LinearLayout level_one = (LinearLayout) findViewById(R.id.menu_one);
-        LinearLayout level_two = (LinearLayout) findViewById(R.id.menu_two);
-        LinearLayout level_three = (LinearLayout) findViewById(R.id.menu_three);
+        level_one = (LinearLayout) findViewById(R.id.menu_one);
+        level_two = (LinearLayout) findViewById(R.id.menu_two);
+        level_three = (LinearLayout) findViewById(R.id.menu_three);
+
+        setLevel();
+
         level_one.setOnClickListener(this);
-        level_two.setOnClickListener(this);
-        level_three.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        setLevel();
     }
 
     @Override
