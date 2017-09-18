f6c14b9dbf787d4f67d2af824643db21f547396a
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 13:00:48 +0700

    add result quiz

--- app/src/main/java/app/magis/meinreisen/QuizResult.java
+++ app/src/main/java/app/magis/meinreisen/QuizResult.java
@@ -1,8 +1,29 @@
 package app.magis.meinreisen;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by rizalsidikp on 17/09/17.
  */
 
-public class QuizResult {
+public class QuizResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView res_quiz = (TextView) findViewById(R.id.res_quiz);
+
+        Intent i = getIntent();
+        Boolean hasil = i.getBooleanExtra("hasilQuiz", true);
+        if(hasil){
+            res_quiz.setText("Berhasil");
+        }else {
+            res_quiz.setText("Gagal");
+        }
+    }
 }
