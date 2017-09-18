ca38d2774e50bfc3bae68e054bb373e977fb487f
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 12:59:28 +0700

    edit learning menu activity

--- app/src/main/java/app/magis/meinreisen/LearningMenu.java
+++ app/src/main/java/app/magis/meinreisen/LearningMenu.java
@@ -15,10 +15,10 @@ public class LearningMenu extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learning_menu);
-        LinearLayout kerja = (LinearLayout) findViewById(R.id.kerja);
-        LinearLayout benda = (LinearLayout) findViewById(R.id.benda);
-        LinearLayout tempat = (LinearLayout) findViewById(R.id.tempat);
+        setContentView(R.layout.activity_menu_vertical);
+        LinearLayout kerja = (LinearLayout) findViewById(R.id.menu_one);
+        LinearLayout benda = (LinearLayout) findViewById(R.id.menu_two);
+        LinearLayout tempat = (LinearLayout) findViewById(R.id.menu_three);
         kerja.setOnClickListener(this);
         benda.setOnClickListener(this);
         tempat.setOnClickListener(this);
@@ -28,15 +28,15 @@ public class LearningMenu extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         Intent i;
         switch (view.getId()){
-            case R.id.kerja:
+            case R.id.menu_one:
                  i = new Intent(LearningMenu.this, LearningKerja.class);
                 startActivity(i);
                 break;
-            case R.id.benda:
+            case R.id.menu_two:
                 i = new Intent(LearningMenu.this, LearningBenda.class);
                 startActivity(i);
                 break;
-            case R.id.tempat:
+            case R.id.menu_three:
                 i = new Intent(LearningMenu.this, LearningTempat.class);
                 startActivity(i);
                 break;
