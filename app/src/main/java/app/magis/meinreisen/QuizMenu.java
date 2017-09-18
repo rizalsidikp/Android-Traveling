e811aef952feb8fbb5aab57b9e8239d296892038
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 13:00:04 +0700

    add quiz drag n drop option

--- app/src/main/java/app/magis/meinreisen/QuizMenu.java
+++ app/src/main/java/app/magis/meinreisen/QuizMenu.java
@@ -10,18 +10,18 @@ import android.widget.LinearLayout;
  * Created by rizalsidikp on 16/09/17.
  */
 
-public class LearningMenu extends AppCompatActivity implements View.OnClickListener {
+public class QuizMenu extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_vertical);
-        LinearLayout kerja = (LinearLayout) findViewById(R.id.menu_one);
-        LinearLayout benda = (LinearLayout) findViewById(R.id.menu_two);
-        LinearLayout tempat = (LinearLayout) findViewById(R.id.menu_three);
-        kerja.setOnClickListener(this);
-        benda.setOnClickListener(this);
-        tempat.setOnClickListener(this);
+        LinearLayout level_one = (LinearLayout) findViewById(R.id.menu_one);
+        LinearLayout level_two = (LinearLayout) findViewById(R.id.menu_two);
+        LinearLayout level_three = (LinearLayout) findViewById(R.id.menu_three);
+        level_one.setOnClickListener(this);
+        level_two.setOnClickListener(this);
+        level_three.setOnClickListener(this);
     }
 
     @Override
@@ -29,15 +29,15 @@ public class LearningMenu extends AppCompatActivity implements View.OnClickListe
         Intent i;
         switch (view.getId()){
             case R.id.menu_one:
-                 i = new Intent(LearningMenu.this, LearningKerja.class);
+                 i = new Intent(QuizMenu.this, QuizTrueFalse.class);
                 startActivity(i);
                 break;
             case R.id.menu_two:
-                i = new Intent(LearningMenu.this, LearningBenda.class);
+                i = new Intent(QuizMenu.this, QuizDragNDrop.class);
                 startActivity(i);
                 break;
             case R.id.menu_three:
-                i = new Intent(LearningMenu.this, LearningTempat.class);
+                i = new Intent(QuizMenu.this, LearningTempat.class);
                 startActivity(i);
                 break;
             default:
