d8a850177533c04dfd024dd9dbc2dcf0bc7466d3
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-18 18:26:45 +0700

    add connector sqlite

--- app/src/main/java/app/magis/meinreisen/Connector.java
+++ app/src/main/java/app/magis/meinreisen/Connector.java
@@ -1,8 +1,37 @@
 package app.magis.meinreisen;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by rizalsidikp on 18/09/17.
  */
 
 public class Connector {
+    private static final String DB_NAME = "meinreisen.db";
+    SQLiteDatabase database;
+    DBOpenHelper DBOpenHelper;
+
+    public Connector(Context context){
+        DBOpenHelper = new DBOpenHelper(context, DB_NAME, null, 1);
+    }
+
+    public void open() throws SQLException {
+        database = DBOpenHelper.getWritableDatabase();
+    }
+
+    public void close(){
+        if(database != null)
+            database.close();
+    }
+
+    public void open_level(int level){
+        ContentValues Levels = new ContentValues();
+        Levels.put("status", 1);
+        open();
+        database.update("levels", Levels, "level = " + level, null);
+        close();
+    }
 }
